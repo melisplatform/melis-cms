d9d3478e244f6c2e7e56c6aa56d13e8be642fe4e
jerremeirago <jrago@melistechnology.com>
2017-10-06 17:40:56 +0800

    updated app.microservice

--- config/app.microservice.php
+++ config/app.microservice.php
@@ -3,59 +3,7 @@ return array(
 	'plugins' => array(
 		'microservice' => array(
 			'MelisCms' => array(
-				//MelisCmsPageGetterService.php
-				'MelisCmsPageGetterService' => array(
-					/**
-					 * @param $pageId
-					 * @method getPageContent()
-					 */
-					'getPageContent' => array(
-						'attributes' => array(
-							'name'	=> 'microservice_form',
-							'id'	=> 'microservice_form',
-							'method'=> 'POST',
-							'action'=> $_SERVER['REQUEST_URI'],
-						),
-						'hydrator' => 'Zend\Stdlib\Hydrator\ArraySerializable',
-						'elements' => array(
-							array(
-								'spec' => array(
-									'name' => 'pageId',
-									'type' => 'Text',
-									'options' => array(
-										'label' => 'Page Id',
-									),
-									'attributes' => array(
-										'id' => 'pageId',
-										'value' => '',
-										'class' => '',
-										'placeholder' => 'Enter Page Id',
-									),
-								),
-							),
-						),
-						'input_filter' => array(
-							'pageId' => array(
-								'name' => 'pageId',
-								'required' => true,
-								'validators' => array(
-									array(
-										'name' => 'IsInt',
-										'options' => array(
-											'message' => array(
-												\Zend\I18n\Validator\IsInt::INVALID => 'PageId must be an integer'
-											),
-										),
-									),
-								),
-								'filters' => array(
-									array('name' => 'StripTags'),
-									array('name' => 'StringTrim')
-								),
-							),
-						),
-					),
-				),
+				
 				'MelisCmsSiteService' => array(
 					/**
 					 * @param $siteId
@@ -82,7 +30,7 @@ return array(
 										'id' => 'siteId',
 										'value' => '',
 										'class' => '',
-										'placeholder' => 'Enter Site Id',
+										'placeholder' => 'Enter siteId',
 									),
 								),
 							),
