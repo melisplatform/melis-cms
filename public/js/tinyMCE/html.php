bd5a9f25e09cc67c1a9625a595ea4311cc9e2be5
James Quibido <ninojamesquibido@gmail.com>
2017-11-23 18:41:43 +0800

    updated tinymce html and media config

--- public/js/tinyMCE/html.php
+++ public/js/tinyMCE/html.php
@@ -1,19 +1,20 @@
 <?php 
 	return array(
-		'relative_urls' => false,
-		'selector' => 'html-editable-selector',
-		'language' => 'en',
-		'inline' => true,
-		'templates'  => 'miniTemplates',
-		'menubar' => false,
-        'forced_root_block'  => 'p',
-		'cleanup'  => false,
-		'verify_html'  => false,
-		'plugins' => array(
-		    'advlist autolink lists link image charmap print preview anchor',
-		    'searchreplace visualblocks code fullscreen',
-		    'insertdatetime media table contextmenu paste template'
-	    ),
-	    'toolbar' => 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media template | code',
-	    'init_instance_callback'  => 'tinyMceCleaner',
+        'relative_urls' => false,
+        'selector' => 'html-editable-selector',
+        'language' => 'en',
+        'inline' => true,
+        'templates' => 'miniTemplates',
+        'menubar' => false,
+        'forced_root_block' => 'p',
+        'cleanup' => false,
+        'verify_html' => false,
+        'plugins' => array(
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table contextmenu paste template wordcount'
+        ),
+        'image_advtab' => true,
+        'toolbar' => 'insertfile undo redo | formatselect | bold italic strikethrough underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media template | code',
+        'init_instance_callback' => 'tinyMceCleaner',
 	); 
--- public/js/tinyMCE/media.php
+++ public/js/tinyMCE/media.php
@@ -1,18 +1,19 @@
 <?php 
 	return array(
-		'relative_urls' => false,
-		'selector' => 'media-editable-selector',
-		'language' => 'en',
-		'inline' => true,
-		'menubar' => false,
-        'forced_root_block'  => 'p',
-		'cleanup'  => false,
-		'verify_html'  => false,
-		'plugins' => array(
-		    'advlist autolink lists link image charmap print preview anchor',
-		    'searchreplace visualblocks code fullscreen',
-		    'insertdatetime media table contextmenu paste'
-	    ),
-	    'toolbar' => 'insertfile undo redo link image media | code',
-	    'init_instance_callback'  => 'tinyMceCleaner',
+        'relative_urls' => false,
+        'selector' => 'media-editable-selector',
+        'language' => 'en',
+        'inline' => true,
+        'menubar' => false,
+        'forced_root_block' => 'p',
+        'cleanup' => false,
+        'verify_html' => false,
+        'plugins' => array(
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table contextmenu paste'
+        ),
+        'image_advtab' => true,
+        'toolbar' => 'insertfile undo redo link image media | code',
+        'init_instance_callback' => 'tinyMceCleaner',
 	); 
