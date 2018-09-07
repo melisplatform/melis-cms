350eafc0e625d80dea332fb509ad554202c5cca9
Franz Joseph Paran <fparan@melistechnology.com>
2018-09-07 17:02:10 +0800

    removed gdpr

--- config/app.tools.php
+++ config/app.tools.php
@@ -2564,28 +2564,5 @@ return array(
                 ),
             ),
         ),
-        'meliscore' => [
-            'tools' => [
-                'melis_core_gdpr_tool' => [
-                    'forms' => [
-                        'melis_core_gdpr_search_form' => [
-                            'elements' => [
-                                [
-                                    'spec' => [
-                                        'name' => 'site_id',
-                                        'type' => 'MelisCoreSiteSelect',
-                                        'options' => [
-                                            'label' => 'tr_melis_core_gdpr_form_site',
-                                            'form_type' => 'form-horizontal',
-                                            'empty_option' => 'tr_meliscore_common_choose',
-                                        ],
-                                    ],
-                                ]
-                            ],
-                        ],
-                    ],
-                ],
-            ],
-        ],
     ),
 );  
