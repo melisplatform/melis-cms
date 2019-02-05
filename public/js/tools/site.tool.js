706b28e97cecb1ab1404df93f3c83b2dba7a238c
Cris <crismakabenta@gmail.com>
2019-02-05 12:56:42 +0800

    Added error modal for minifying assets

--- public/js/tools/site.tool.js
+++ public/js/tools/site.tool.js
@@ -331,9 +331,10 @@ $(document).ready(function() {
 		});
 	});
 
-    // Add Event to "Delete Button"
+    // Add Event to "Minify Button"
     addEvent(".btnMinifyAssets", function(e) {
         var siteId = $(this).parents("tr").attr("id");
+        var _this = $(this);
 		$.ajax({
 			type        : 'POST',
 			url         : '/minify-assets',
@@ -341,10 +342,28 @@ $(document).ready(function() {
 			dataType    : 'json',
 			encode		: true,
 			beforeSend  : function(){
-
+                _this.attr('disabled', true);
 			},
 			success		: function(data){
-				melisHelper.melisKoNotification(data.title, '', data.errors);
+				var errorTexts = '<h3>'+ melisHelper.melisTranslator(data.title) +'</h3>';
+				$.each( data.errors, function( key, error ) {
+					$.each( error, function( key, value ) {
+                        errorTexts += '<p class="modal-error-cont"><b>'+ key + ': </b>  ';
+						if(value instanceof Object){
+							$.each(value, function(key, val){
+                                errorTexts += '<span><i class="fa fa-circle"></i>'+ '<label>'+ key + ': </label>  ' + val + '</span>';
+							})
+						}else{
+                            errorTexts += '<span><i class="fa fa-circle"></i>'+ value + '</span>';
+						}
+					});
+					errorTexts += '</p>';
+				});
+				var div = "<div class='melis-modaloverlay overlay-hideonclick'></div>";
+				div += "<div class='melis-modal-cont KOnotif'>  <div class='modal-content'>"+ errorTexts +" <span class='btn btn-block btn-primary'>"+ translations.tr_meliscore_notification_modal_Close +"</span></div> </div>";
+				$body.append(div);
+
+                _this.attr('disabled', false);
 			}
 		});
     });
