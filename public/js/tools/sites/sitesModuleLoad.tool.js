231bff98384caf380a128d082b904a079212c46e
Salcover <simonalcover@gmail.com>
2019-02-21 18:13:04 +0800

    updated site v2 module loading tab switches

--- public/js/tools/sites/sitesModuleLoad.tool.js
+++ public/js/tools/sites/sitesModuleLoad.tool.js
@@ -6,17 +6,17 @@ $(document).ready(function() {
      * This block of code handles the main switch in site module loading
      * where will switch on/off all modules.
      */
-    if($("#select-deselect-all-module").length > 0){
-        $body.on("switch-change", "#select-deselect-all-module", function(e, data){
+    $body.on("switch-change", "#site-select-deselect-all-module", function(e, data){
 
-            var val = "";
-            if(data.value === false){
-                val = "off";
-            }else{
-                val = "on";
-            }
-            $('.module-switch').find("div.switch-animate").removeClass("switch-on switch-off").addClass("switch-"+val);
-        });
-    }
+        var currentTabId = activeTabId.split("_")[0];
+        var val = "";
+        if(data.value === false){
+            val = "off";
+        }else{
+            val = "on";
+        }
+
+        $('.'+currentTabId+'_module_switch').find('div.switch-animate').removeClass("switch-on switch-off").addClass("switch-"+val);
+    });
 
 });
