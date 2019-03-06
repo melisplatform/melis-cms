79f953c85d445b89a13e89d8b869736c25b73a32
jerremeirago <jrago@melistechnology.com>
2019-03-06 16:53:46 +0800

    fixed plugin size error

--- public/js/plugins/plugins.edition.js
+++ public/js/plugins/plugins.edition.js
@@ -791,11 +791,11 @@ var melisPluginEdition = (function($, window) {
     }
 
     // remove inline width when changing viewport
-    window.parent.$("#"+ parent.activeTabId).find('iframe').on("resize", function() {
-        $(this).contents().find(".melis-dragdropzone .melis-ui-outlined").each(function() {
-            $(this).css("width", "");
-        });
-    });
+    // window.parent.$("#"+ parent.activeTabId).find('iframe').on("resize", function() {
+    //     $(this).contents().find(".melis-dragdropzone .melis-ui-outlined").each(function() {
+    //         $(this).css("width", "");
+    //     });
+    // });
 
     // init resize
     if (parent.pluginResizable == 1) {
