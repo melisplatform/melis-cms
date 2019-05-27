92558a256625d40e2b190cb8e3662e6f470a86ee
Cris <crismakabenta@gmail.com>
2019-05-27 16:33:53 +0800

    Added section on plugin config datas

--- config/dashboard-plugins/MelisCmsPagesIndicatorsPlugin.config.php
+++ config/dashboard-plugins/MelisCmsPagesIndicatorsPlugin.config.php
@@ -46,6 +46,19 @@
                             'width' => 6,
                             'x-axis' => 0,
                             'y-axis' => 0,
+                            /*
+                             * if set this plugin will belong to a specific marketplace section,
+                               * if not it will go directly to ( Others ) section
+                               *  - available section for dashboard plugins as of 2019-05-16
+                               *    - MelisCore
+                               *    - MelisCms
+                               *    - MelisMarketing
+                               *    - MelisSite
+                               *    - MelisCommerce
+                               *    - Others
+                               *    - CustomProjects
+                             */
+                            'section' => 'MelisCms',
                         ],
                         'forward' => array(
                             'module' => 'MelisCms',
