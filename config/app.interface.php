944e8e7ce9961a64f75853c98ced0261233bab6d
jun <junry.zabate@gmail.com>
2020-07-08 17:49:31 +0800

    fix for issue on sites tools

--- config/app.interface.php
+++ config/app.interface.php
@@ -131,7 +131,7 @@ return array(
                  * lesser requests
                  */
                 'build' => [
-                    'disable_bundle' => true,
+                    'disable_bundle' => false,
                     // lists of assets that will be loaded in the layout
                     'css' => [
                         '/MelisCms/build/css/bundle.css',
