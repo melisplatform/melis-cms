1b2f4b564c4f249858d7cd04661c54aeae06cca4
Maria Teresa Pomar <mpomar@melistechnology.com>
2021-11-26 12:08:58 +0800

    fix tickets

--- src/Form/Factory/MelisCmsLanguageSelectFactory.php
+++ src/Form/Factory/MelisCmsLanguageSelectFactory.php
@@ -19,14 +19,41 @@ class MelisCmsLanguageSelectFactory extends MelisSelectFactory
 {
 	protected function loadValueOptions(ServiceManager $serviceManager)
 	{
+		
 		$tableLang = $serviceManager->get('MelisEngineTableCmsLang');
 		$languages = $tableLang->fetchAll();
-		
+		$siteLangNames = null;
+
+		//during page creation, language options should only include the languages that were set for the given site
+		$request = $serviceManager->get('request');
+		$idPage = $request->getQuery('idPage');
+		$idFatherPage = $request->getQuery('idFatherPage');
+
+		if (empty($idPage) && $idFatherPage) {
+			//get site id using the father page id
+		    $melisPage = $serviceManager->get('MelisEnginePage');
+	        $datasPage = $melisPage->getDatasPage($idFatherPage, 'published');  
+	        if ($datasPage->getMelisTemplate()) {
+	        	$siteId = $datasPage->getMelisTemplate()->tpl_site_id;
+
+				$siteLangsTable = $serviceManager->get('MelisEngineTableCmsSiteLangs');
+		        $siteLangs = $siteLangsTable->getSiteLangs(null, $siteId, null, true)->toArray();	
+
+		        if ($siteLangs) {
+		        	$siteLangNames = array_column($siteLangs,'lang_cms_name');
+		        }		        
+	        }			
+		}
+
 		$valueoptions = [];
 		$max = $languages->count();
 		for ($i = 0; $i < $max; $i++) {
 			$tpl = $languages->current();
-			$valueoptions[$tpl->lang_cms_id] = $tpl->lang_cms_name;
+
+			if ( (!empty($siteLangNames) && in_array($tpl->lang_cms_name, $siteLangNames)) || empty($siteLangNames) ) {
+				$valueoptions[$tpl->lang_cms_id] = $tpl->lang_cms_name;				
+			}	
+
 			$languages->next();
 		}
 		
