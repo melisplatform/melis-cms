168796aabe9c19b64c3dad35c040fd8abdd4139d
jzabate <jzabate@melistechnology.com>
2024-09-03 17:19:54 +0800

    check issue on dragndrop.js

--- public/js/dragndrop/dragndrop.js
+++ public/js/dragndrop/dragndrop.js
@@ -212,7 +212,7 @@ var melisDragnDrop = (function ($, window) {
 							function (e) {
 								setTimeout(function () {
 									// check if loader exists
-									//console.log(`$(ui.helper[0]).parent().find(".overlay-loader").length: `, $(ui.helper[0]).parent().find(".overlay-loader").length);
+									console.log(`$(ui.helper[0]).parent().find(".overlay-loader").length: `, $(ui.helper[0]).parent().find(".overlay-loader").length);
 									if (
 										!$(ui.helper[0]).parent().find(".overlay-loader").length
 									) {
