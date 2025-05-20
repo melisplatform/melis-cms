3e0039275ea26eedeb4b5e2429357611eedaba9f
jzabate <jzabate@melistechnology.com>
2025-05-20 20:29:43 +0800

    edit on dynamic-dragndrop.js and css

--- public/js/dragndrop/dynamic-dragndrop.js
+++ public/js/dragndrop/dynamic-dragndrop.js
@@ -1,60 +1,51 @@
-var melisDynamicDragnDrop = (function($, window) {
-    function init() {
-        let $iconButtons    = $(".dnd-layout-buttons .column-icon"),
-            $dndButtons     = $('.dnd-layout-buttons, .dnd-bottom-buttons'),
-            $dndWrapper     = $(".dnd-layout-wrapper");
-            
-            // .dnd-layout-wrapper, mouseenter & mouseleave, mouseover & mouseout
-            $dndWrapper
-                .on("mouseenter", function(e) {
-                    //e.stopPropagation();
-
-                    $dndButtons.removeClass("show-buttons");
-
-                    $(this).children(".dnd-layout-buttons, .dnd-bottom-buttons").addClass("show-buttons");
-                    /* $(this).children(".dnd-layout-buttons, .dnd-bottom-buttons").fadeIn("400", "slow", function() {
-                        $(this).css("opacity", 1);
-                    }); */
-                })
-                .on("mouseleave", function() {
-                    $(this).children(".dnd-layout-buttons, .dnd-bottom-buttons").removeClass("show-buttons");
-                    /* $(this).children(".dnd-layout-buttons, .dnd-bottom-buttons").fadeOut("400", "slow", function() {
-                        $(this).css("opacity", 0);
-                    }); */
-                });
-
-            // .dnd-layout-buttons
-            $.each($iconButtons, function(i, v) {
-                let $iconButton = $(v);
-                    $iconButton
-                        .on("mouseover", function() {
-                            $(this).find(".icon-col-bg").removeClass("bg-white").addClass("bg-red");
-                        })
-                        .on("mouseout", function() {
-                            $(this).find(".icon-col-bg").addClass("bg-white").removeClass("bg-red");
-                        });
-            });
-    }
-
-    return {
-        init : init
-    };
-})(jQuery, window);
-
 $(function() {
-    melisDynamicDragnDrop.init();
+    let $body       = $("body"),
+        $dndButtons = $('.dnd-layout-buttons, .dnd-bottom-buttons');
 
-    let $body = $("body");
-        $body.on("click", ".dnd-plus-button", function() {          
+        // .dnd-layout-wrapper
+        $body
+            .on("mouseenter", ".dnd-layout-wrapper", function(e) {
+                // e.stopPropagation();
+
+                //$dndButtons.removeClass("show-buttons");
+                $dndButtons.hide();
+
+                //$(this).children(".dnd-layout-buttons, .dnd-bottom-buttons").addClass("show-buttons");
+                $(this).children(".dnd-layout-buttons").show();
+
+                $(this).children(".dnd-bottom-buttons").css({
+                    "opacity" : "1",
+                    "visibility" : "visible"
+                });
+            })
+            .on("mouseleave", ".dnd-layout-wrapper", function() {
+                //$(this).children(".dnd-layout-buttons, .dnd-bottom-buttons").removeClass("show-buttons");
+                $(this).children(".dnd-layout-buttons").hide();
+
+                $(this).children(".dnd-bottom-buttons").css({
+                    "opacity" : "0",
+                    "visibility" : "hidden"
+                });
+            });
+        
+        // .column-icons, button tag
+        $body
+            .on("mouseenter", ".column-icon", function() {
+                $(this).find(".icon-col-bg").removeClass("bg-white").addClass("bg-red");
+            })
+            .on("mouseleave", ".column-icon", function() {
+                $(this).find(".icon-col-bg").addClass("bg-white").removeClass("bg-red");
+            });
+
+        // .dnd-plus-button
+        /* $body.on("click", ".dnd-plus-button", function() {          
             let _this = $(this);
             let pluginId = _this.data("pluginId");
             let parentDNDId = _this.closest("melis-dragdropzone-container").last();
-        });
+        }); */
 
+        // .dnd-layout-buttons
         $body.on("click", ".dnd-layout-buttons div[data-dnd-tpl]", function() {
-            // initialize dynamic dragndrop
-            melisDynamicDragnDrop.init();
-
             let dndId = $(this).data("dndId");
             let dndTpl = $(this).data("dndTpl");
             let pageId = $(this).data("pageId");
@@ -72,8 +63,7 @@ $(function() {
                 dndId,
                 dndTpl,
                 melisSite
-            })
-            .done((res) => {
+            }).done((res) => {
                 //console.log({res});
                 if (res.success) {
                     $(this).find("#loader").remove();
@@ -101,6 +91,8 @@ $(function() {
                     }
 
                 }
+            }).always(() => {
+                $(this).find("#loader").remove();
             });
         });
 });
